b6dbfb85b8939ea5e2a5b73cc005544f6df73e7a
dangolbeeker <magiver273@gmail.com>
2021-02-09 16:58:06 -0500

    stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Welcome to my App',
       home: Scaffold(
@@ -12,28 +14,22 @@ class MyApp extends StatelessWidget {
           title: Text('Welcome to My App'),
         ),
         body: Center(
-          child: Text('Hello Flutter'),
+          child: Text(wordPair.asPascalCase),
         ),
       ),
     );
   }
 }
 
-// void main() => runApp(MyApp());
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
 
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Welcome to Flutter',
-//       home: Scaffold(
-//         appBar: AppBar(
-//           title: Text('Welcome to Flutter'),
-//         ),
-//         body: Center(
-//           child: Text('Hello World'),
-//         ),
-//       ),
-//     );
-//   }
-// }
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
